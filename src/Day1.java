2458a286921328a1fac998d8bd70b11c8bb9fe94
fangtastix <syb16192@uni.strath.ac.uk>
2020-12-02 15:46:11 +0000

    Adding everything...again lol

---
+++ src/Day1.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day1 {
+
+    static ArrayList<Integer> data = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        try {
+            File dataInp = new File("data/day1input.txt");
+            Scanner sc = new Scanner(dataInp);
+            while(sc.hasNextLine()){
+                data.add(sc.nextInt());
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+
+        System.out.println("Result: " + calcResult());
+    }
+
+    public static int calcResult() {
+        for(int i = 0; i < data.size(); i++) {
+            for(int j = 0; j < data.size(); j++) {
+                for(int k = 0; k < data.size(); k++) {
+                    int add = data.get(i) + data.get(j) + data.get(k);
+                    if (add == 2020) {
+                        return data.get(i) * data.get(j) * data.get(k);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/Day2.java
@@ -0,0 +1,99 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day2{
+
+    public static void main(String[] args) {
+        try {
+            File dataInp = new File("data/day2input.txt");
+            Scanner sc = new Scanner(dataInp);
+            int counter = 0;
+            while(sc.hasNextLine()){
+                if(evalLinePart2(sc.nextLine())) {
+                    counter++;
+                }
+            }
+
+            System.out.println("Result: " + counter);
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+    public static boolean evalLinePart1(String line){
+        int min = 0;
+        int max = 0;
+        char letter = ' ';
+        String password = "";
+        int lCounter = 0;
+
+        int hIndex = 0;
+        int spaces = 0;
+
+        for(int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+
+            if(c == '-') {
+                min = Integer.parseInt(line.substring(0, i));
+                hIndex = i;
+            }
+            else if(c == ' ' && spaces == 0){
+                spaces++;
+                max = Integer.parseInt(line.substring(hIndex + 1, i));
+                letter = line.charAt(i + 1);
+            }
+            else if(c == ' ' && spaces == 1){
+                password = line.substring(i + 1);
+            }
+        }
+
+        for(int i = 0; i < password.length(); i++){
+            char c = password.charAt(i);
+            if(c == letter){
+                lCounter++;
+            }
+        }
+
+        return !((lCounter > max) || (lCounter < min));
+    }
+
+    public static boolean evalLinePart2(String line){
+        int i1 = 0;
+        int i2 = 0;
+        char letter = ' ';
+        String password = "";
+
+        int hIndex = 0;
+        int spaces = 0;
+
+        for(int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+
+            if(c == '-') {
+                i1 = Integer.parseInt(line.substring(0, i));
+                hIndex = i;
+            }
+            else if(c == ' ' && spaces == 0){
+                spaces++;
+                i2 = Integer.parseInt(line.substring(hIndex + 1, i));
+                letter = line.charAt(i + 1);
+            }
+            else if(c == ' ' && spaces == 1){
+                password = line.substring(i + 1);
+            }
+        }
+
+        int count = 0;
+        if(password.charAt(i1 - 1) == letter){
+            count++;
+        }
+
+        if(password.charAt(i2 - 1) == letter){
+            count++;
+        }
+
+        return count == 1;
+    }
+}
