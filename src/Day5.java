46b54a7795ec40d1928af403e7c0923127d98235
calumcdonald <calumcdonald@gmail.com>
2020-12-05 15:34:54 +0000

    Finished day 5. Ignore the out of bounds exception hehe

--- src/Day5.java
+++ src/Day5.java
@@ -1,22 +1,31 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day5{
 
-    public int evalRow(String row){
-        int upper = 127;
-        int lower = 0;
+    public int evalCode(String str, int lowerLimit, int upperLimit){
+        double upper = upperLimit;
+        double lower = lowerLimit;
 
-        for(int i = 0;i < row.length(); i++){
-            char c = row.charAt(i);
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
 
-            if(c == 'F'){
-                upper = upper/2;
+            if(c == 'F' || c == 'L'){
+                upper = Math.floor((upper+lower)/2);
+
+                if(upper == lower){
+                    return (int)upper;
+                }
             }
-            else if(c == 'B'){
-                lower = lower/2;
+            else if(c == 'B' || c == 'R'){
+                lower = Math.ceil((upper+lower)/2);
+
+                if(upper == lower){
+                    return (int)lower;
+                }
             }
         }
         return 0;
@@ -25,6 +34,8 @@ public class Day5{
     public static void main(String[] args) {
         int count = 0;
         int max = 0;
+
+        ArrayList<Integer> seats = new ArrayList<>();
 
         try {
             File dataInp = new File("data/day5input.txt");
@@ -35,22 +46,24 @@ public class Day5{
                 String column = line.substring(7);
                 count++;
 
-                int result = (new Day5().evalRow(row) * 8) /*+ new Day5().evalColumn(column)*/;
+                int result = (new Day5().evalCode(row, 0, 127) * 8) + new Day5().evalCode(column, 0, 7);
+                seats.add(result);
                 if(result > max){
                     max = result;
                 }
-
-                System.out.println("~~~");
-                System.out.println(count);
-                System.out.println("line: " + line);
-                System.out.println("row:" + row);
-                System.out.println("column: " + column);
             }
         }
         catch(FileNotFoundException e) {
             System.out.println("File not found.");
         }
 
-        System.out.println("Result: " + max);
+        System.out.println("Max seat ID: " + max);
+
+        Collections.sort(seats);
+        for(int i = 0; i < max; i++){
+            if(seats.get(i + 1) - seats.get(i) != 1){
+                System.out.println("Your seat: " + (seats.get(i) + 1));
+            }
+        }
     }
 }
